0fbaee58a65bebe8b37e073d32bff7ff8e272f3f
Jose <caoss2022@gmail.com>
2024-01-04 02:11:27 -0500

    agregamos rutas react router en nuestro app

--- src/CardApp.jsx
+++ src/CardApp.jsx
@@ -1,3 +1,4 @@
+import {Navigate, Route, Routes} from 'react-router-dom';
 import {CardView} from './components/CardView';
 import {CatalogView} from './components/CatalogView';
 import {useItemCart} from './hooks/useItemsCart';
@@ -10,19 +11,26 @@ const CardApp=()=>{
     <>
       <div className="container my-4">
         <h3>Card Ap</h3>
-
-        {/* Producto */}
-        <CatalogView handler={ handlerAddProductCart }  />
-
-        {/* Tabla*/}
-        {
-          cardItems?.length <= 0 ||
-            (
-            <CardView
-              items={cardItems}
-              handlerDelete={handlerDeleteProductCard} />
-            )
-        }
+        <Routes>
+          <Route
+            path='catalog'
+          element={ <CatalogView handler={ handlerAddProductCart }/> }>
+          </Route>
+          <Route
+            path='cart'
+            element={
+              (cardItems?.length <= 0) ? (
+                <div className='alert alert-warning'>No hay productos en el carrito de compras</div>
+              )  :
+                ( <CardView
+                  items={cardItems}
+                  handlerDelete={handlerDeleteProductCard} />
+                )
+            }
+          >
+          </Route>
+          <Route path='/' element={ <Navigate to={'/catalog'}/> }></Route>
+        </Routes>
       </div>
     </>
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
 import CardApp from './CardApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CardApp />
+    <BrowserRouter>
+      <CardApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
