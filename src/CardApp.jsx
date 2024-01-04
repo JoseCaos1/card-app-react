fcc1b807702746d5216452e825a1d267afe8c86d
Jose <caoss2022@gmail.com>
2024-01-04 00:11:36 -0500

    actualizamos la cantidad del producto

--- src/CardApp.jsx
+++ src/CardApp.jsx
@@ -13,7 +13,6 @@ const initialCardItems =[
 //      price: 1000
 //    },
 //    quantity: 0,
-//    total: 0
 //  }
 ]
 
@@ -22,14 +21,47 @@ const CardApp=()=>{
   const [ cardItems, setCardItems ] = useState(initialCardItems)
 
   const handlerAddProductCart = (product) =>{
-    setCardItems([
-      ...cardItems,
-      {
-        product,
-        quantity: 1,
-        total: product.price * 1
-      }
-    ]);
+
+    //Para ver si ya existe un id igual en el arreglo
+    const hasItem = cardItems.find((i)=>i.product.id ===product.id);
+    //console.log(hasItem);
+    if(hasItem){
+      //1ER FORMA
+      //{/*Filter lo q hace es lo elimina y lo vuelve a crear por eso es q se sea al final*/}
+      //setCardItems([
+        //...cardItems.filter( (i)=> i.product.id !== product.id ),
+        //{
+          //product,
+          //quantity: hasItem.quantity +1,
+        //}
+      //])
+
+      //2DA FORMA
+      //{/* lo q hace map es lo vuelve a mapear y siempre se debe de volver un return */}
+      setCardItems(
+        cardItems.map( (i)=>{
+          if( i.product.id === product.id){
+            i.quantity = i.quantity+1;
+          }
+          return i;
+        } ),
+        {
+          product,
+          quantity: hasItem.quantity +1,
+        }
+      )
+    }else{
+
+      setCardItems([
+        ...cardItems,
+        {
+          product,
+          quantity: 1,
+        }
+      ]);
+    }
+
+
   }
 
   return(
